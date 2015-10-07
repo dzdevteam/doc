c867492c04b35ce00281162fb6ac1c5eaab6b989
Lê Hoàng Phương <herophuong93@gmail.com>
2015-10-07 15:00:37 +0700

    ajenti guide

--- _entries/03-server-administration.md
+++ _entries/03-server-administration.md
@@ -5,3 +5,4 @@ is-parent: yes
 title: Quản trị máy chủ
 ---
 
+**Kiến thức cần thiết:** Sử dụng dòng lệnh Linux
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
+    // Generate nav for h4
+    $('section.h3').each(function() {
+        var $h3 = $(this),
+            $h3_nav = $('a[href=#' + $h3.attr('id') + ']').parent(),
+            $ul = null;
+        
+        $h3.find('h4').each(function() {
+            if ($h3_nav.find('ul').length == 0) {
+                $ul = jQuery('<ul></ul>');
+                $h3_nav.append($ul);
+                $h3_nav.addClass('parent');
+            }
+            $ul.append('<li><a href="#' + $(this).attr('id') + '">' + $(this).text() + '</a></li>');
+        });
+    });
     $('#nav').onePageNav();
 
     $('a[href^="http"]').attr('target','_blank');
